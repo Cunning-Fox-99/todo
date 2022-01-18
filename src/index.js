7065f8c05c2d6c52a926a4a522339cf29f2f082a
Cunning-Fox-99 <kostya.moroz.96@gmail.com>
2022-01-18 18:58:34 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const TodoList = () => {
+    const items = ['Learn React', 'Build Awesome App']
+    return (
+        <ul>
+            <li>{items[0]}</li>
+            <li>{items[1]}</li>
+        </ul>
+    )
+}
+
+const AppHeader = () => {
+    return (
+        <h1> My Todo List</h1>
+    )
+}
+
+const SearchPanel = () => {
+
+    const searchText = 'Type here to search'
+
+    return (
+        <input type="text" placeholder={searchText} />
+    )
+}
+
+const App = () => {
+
+    const isLoggedIn = true;
+    const loginBox = <span>Log in please</span>
+    const welcomeBox = <span>Welcome Back</span>
+
+    return (
+        <div>
+            <span> {isLoggedIn ? welcomeBox : loginBox}</span>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList />
+        </div>
+    )
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.getElementById('root')
+)
