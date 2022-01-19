a6853568b6bedcda18244210584e81e1f3ef29b7
Cunning-Fox-99 <kostya.moroz.96@gmail.com>
2022-01-19 16:24:09 +0200

    fixes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app';
 
-import TodoList from "./components/todo-list";
-import AppHeader from "./components/app-header";
-import SearchPanel from "./components/search-panel";
-
-const App = () => {
-
-    const isLoggedIn = true;
-    const loginBox = <span>Log in please</span>
-    const welcomeBox = <span>Welcome Back</span>
-
-    return (
-        <div>
-            <span> {isLoggedIn ? welcomeBox : loginBox}</span>
-            <AppHeader />
-            <SearchPanel />
-            <TodoList />
-        </div>
-    )
-}
-
-
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-)
+ReactDOM.render(<App />,
+  document.getElementById('root'));
